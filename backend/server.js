6ce39a0656f8e922cf97e5ef2cba92b81560ffa9
bosirullah <mdbosirullah@gmail.com>
2024-06-04 16:42:11 +0530

    for deployment coded

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const authRoutes = require("./routes/auth");
 // const userRoutes = require("./routes/userRoutes");
 const connectDB = require("./config/db");
 const router = require("./routes/auth");
+const path = require("path");
 
 dotenv.config();
 
@@ -25,6 +26,24 @@ app.use(express.json());
 
 app.use("/events", eventRoutes);
 app.use("/auth", authRoutes);
+
+// --------------------- Deployment --------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/src/app")));
+
+    app.get("*", (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname1, "frontend", "src", "app", "page.tsx")
+        )
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running..");
+    });
+}
 
 // app.use("/users", userRoutes);
 
